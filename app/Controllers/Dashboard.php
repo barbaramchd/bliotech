12c30d128a3506f856f4d871e92712f215f0ac47
jjur <juraj.vasek@minerva.kgi.edu>
2021-05-09 03:02:30 +0200

    Migrating html files to php

--- app/Controllers/Dashboard.php
+++ app/Controllers/Dashboard.php
@@ -6,8 +6,45 @@ class Dashboard extends BaseController
 {
 	public function index()
 	{
+	    global $DATA;  # Contains information about logged user to be used in view
+
 	    die("You are magic!");
         //return redirect()->to('/Dashboard/');
 		//return view('welcome_message');
 	}
+
+	public function accounts()  # For demo purposes
+    {
+        echo view("dashboard/header");
+        # Content
+        echo view("dashboard/footer");
+    }
+
+    public function admin()  # For demo purposes
+    {
+        echo view("dashboard/header");
+        # Content
+        echo view("dashboard/footer");
+    }
+
+    public function home() # For demo purposes
+    {
+        echo view("dashboard/header");
+        # Content
+        echo view("dashboard/footer");
+    }
+
+    public function restaurant() # For demo purposes
+    {
+        echo view("dashboard/header");
+        # Content
+        echo view("dashboard/footer");
+    }
+
+    public function settings() # For demo purposes
+    {
+        echo view("dashboard/header");
+        # Content
+        echo view("dashboard/footer");
+    }
 }

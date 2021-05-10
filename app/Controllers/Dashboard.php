16d7898e74e8bc9bd125ea6eab2b917234845b28
jjur <juraj.vasek@minerva.kgi.edu>
2021-05-11 00:10:59 +0200

    Loaded $DATA for every view

--- app/Controllers/Dashboard.php
+++ app/Controllers/Dashboard.php
@@ -15,36 +15,73 @@ class Dashboard extends BaseController
 
 	public function accounts()  # For demo purposes
     {
-        echo view("dashboard/header");
-        echo view("dashboard/accounts");
-        echo view("dashboard/footer");
+        global $DATA;
+
+        # Displaying Header
+        echo view("dashboard/header", $DATA);
+        echo view("dashboard/accounts", $DATA);
+        echo view("dashboard/footer", $DATA);
     }
 
     public function admin()  # For demo purposes
     {
-        echo view("dashboard/header");
+        global $DATA;
+
+        # Displaying Header
+        echo view("dashboard/header", $DATA);
         # Content
-        echo view("dashboard/footer");
+        echo view("dashboard/footer", $DATA);
     }
 
     public function home() # For demo purposes
     {
-        echo view("dashboard/header");
-        echo view("dashboard/home");
-        echo view("dashboard/footer");
+        global $DATA;
+
+        # Displaying Header
+        echo view("dashboard/header", $DATA);
+
+        # IF current user is manager
+        if (($DATA["isManager"] == true) OR ($DATA["isAdmin"] == true)){
+            echo view("dashboard/home", $DATA);
+        }else{
+            # IF current user is worker
+            # TODO redirect to single unit
+            # Find which unit is associated with the worker
+
+
+            return redirect()->to('/Dashboard/unit/'+$unit_id);
+        }
+
+        echo view("dashboard/footer", $DATA);
+    }
+
+    public function unit($unit_id) # For demo purposes
+    {
+        global $DATA;
+
+        # Displaying Header
+        echo view("dashboard/header", $DATA);
+        echo view("dashboard/home", $DATA);
+        echo view("dashboard/footer", $DATA);
     }
 
     public function units() # For demo purposes
     {
-        echo view("dashboard/header");
-        echo view("dashboard/units");
-        echo view("dashboard/footer");
+        global $DATA;
+
+        # Displaying Header
+        echo view("dashboard/header", $DATA);
+        echo view("dashboard/units", $DATA);
+        echo view("dashboard/footer", $DATA);
     }
 
     public function settings() # For demo purposes
     {
-        echo view("dashboard/header");
+        global $DATA;
+
+        # Displaying Header
+        echo view("dashboard/header", $DATA);
         # Content
-        echo view("dashboard/footer");
+        echo view("dashboard/footer", $DATA);
     }
 }

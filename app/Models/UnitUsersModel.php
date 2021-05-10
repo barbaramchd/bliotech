d2226dd481087cb4e3a213cfe37fc6f5039a4831
jjur <juraj.vasek@minerva.kgi.edu>
2021-05-11 00:31:31 +0200

    Adding method to the model

--- app/Models/UnitUsersModel.php
+++ app/Models/UnitUsersModel.php
@@ -7,10 +7,22 @@ use CodeIgniter\Model;
 class UnitUsersModel extends Model
 {
     protected $table = 'unit_users';
-    protected $allowedFields = ['unit_id', 'device_id'];
+    protected $allowedFields = ['unit_id', 'user_id'];
     protected $useTimestamps = false;
 
+    public function getUnit($user_id)
+    {
+        return $this->asArray()
+            ->where(['user_id' => $user_id])
+            ->first();
+    }
 
+    public function getUser($unit_id)
+    {
+        return $this->asArray()
+            ->where(['unit_id' => $unit_id])
+            ->first();
+    }
     
 
 }

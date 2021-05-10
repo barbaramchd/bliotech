69ae95d9291d0abde6ac7f9dfe0ef2f8cce6a231
jjur <juraj.vasek@minerva.kgi.edu>
2021-05-11 01:32:47 +0200

    name column renamed

--- app/Models/UnitsModel.php
+++ app/Models/UnitsModel.php
@@ -7,7 +7,7 @@ use CodeIgniter\Model;
 class UnitsModel extends Model
 {
     protected $table = 'units';
-    protected $allowedFields = ['name', 'company', "address1", "address2", "city", "state", "zip",
+    protected $allowedFields = ['unit_name', 'company', "address1", "address2", "city", "state", "zip",
                                 "country", "phone", "email", "website","type","size","active"];
     protected $useTimestamps = true;
     protected $createdField  = 'created_at';

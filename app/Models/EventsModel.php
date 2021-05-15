ba4e0560cd190e3bf543f20d93db741a66fe099e
jjur <juraj.vasek@minerva.kgi.edu>
2021-05-16 01:53:20 +0200

    updated DB structure

--- app/Models/EventsModel.php
+++ app/Models/EventsModel.php
@@ -7,17 +7,16 @@ use CodeIgniter\Model;
 class EventsModel extends Model
 {
     protected $table = 'events';
-    protected $allowedFields = ['e_type', 'e_device_id', "e_note"];
+    protected $allowedFields = ['e_type', 'e_device_id', "e_note", "e_resolved_at", "e_resolved_by"];
     protected $useTimestamps = true;
     protected $createdField  = 'e_created_at';
     protected $updatedField  = 'e_updated_at';
     protected $deletedField  = 'e_deleted_at';
     protected $primaryKey = 'e_id';
 
-    public function getLatestByDevice($device){
-
+    public function getLatestByDevice($device_id){
         return $this->asArray()
-            ->where(['e_device_id' => $device])
+            ->where(['e_device_id' => $device_id])
             ->orderBy("e_created_at","DESC")
             ->first();
     }

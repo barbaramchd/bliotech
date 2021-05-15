21976edb4a2806e5981d32742427cc21a55a84dd
barbaramachado.dex@gmail.com <barbaramachado.dex@gmail.com>
2021-05-15 12:54:17 -0700

    added rules and messages into validation

--- app/Views/dashboard/units.php
+++ app/Views/dashboard/units.php
@@ -10,128 +10,134 @@
                 Add new unit
             </button>
             <div class="modal fade" id="modal-xl" style="display: none;" aria-hidden="true">
-                <div class="modal-dialog modal-xl">
-                    <div class="modal-content">
-                        <div class="modal-header">
-                            <h4 class="modal-title">New Unit Info</h4>
-                            <button type="button" class="close" data-dismiss="modal" aria-label="Close">
-                                <span aria-hidden="true">×</span>
-                            </button>
-                        </div>
-                        <div class="modal-body">
-                            <div class="row">
-                                <div class="col-md-6">
-                                    <div class="form-group">
-                                        <label>Unit's name</label>
-                                        <input type="text" name="u_name" class="form-control" placeholder="Enter name"
-                                               required="required">
+                <form id="units-form">
+                    <div class="modal-dialog modal-xl">
+                        <div class="modal-content">
+                            <div class="modal-header">
+                                <h4 class="modal-title">New Unit Info</h4>
+                                <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                                    <span aria-hidden="true">×</span>
+                                </button>
+                            </div>
+                            <div class="modal-body">
+                                <div class="row">
+                                    <div class="col-md-6">
+                                        <div class="form-group">
+                                            <label>Unit's name</label>
+                                            <input type="text" name="u_name" id="u_name" class="form-control"
+                                                   placeholder="Enter name"
+                                                   required>
+                                        </div>
+                                        <div class="form-group">
+                                            <label>Company</label>
+                                            <input type="text" name="u_company" id="u_company" class="form-control"
+                                                   placeholder="Enter company">
+                                        </div>
                                     </div>
-                                    <div class="form-group">
-                                        <label>Company</label>
-                                        <input type="text" name="u_company" class="form-control"
-                                               placeholder="Enter company">
+                                    <div class="col-md-6">
+                                        <div class="form-group">
+                                            <label>Address 1</label>
+                                            <input type="text" name="u_address1" id="u_address1" class="form-control"
+                                                   placeholder="Enter address"
+                                                   required>
+                                        </div>
+                                        <div class="form-group">
+                                            <label>Address 2</label>
+                                            <input type="text" name="u_address2" id="u_address2" class="form-control"
+                                                   placeholder="Enter address">
+                                        </div>
+                                    </div>
+                                    <div class="col-md-4">
+                                        <div class="form-group">
+                                            <label>City</label>
+                                            <input type="text" name="u_city" id="u_city" class="form-control"
+                                                   placeholder="Enter city"
+                                                   required>
+                                        </div>
+                                    </div>
+                                    <div class="col-md-2">
+                                        <div class="form-group">
+                                            <label>State</label>
+                                            <select class="form-control select2 select2-hidden-accessible"
+                                                    style="width: 100%;"
+                                                    data-select2-id="1" tabindex="-1" aria-hidden="true"
+                                                    id="u_state" name="u_state">
+                                                <option selected="selected"> --Select--</option>
+                                                <option data-select2-id="115">Option 1</option>
+                                            </select>
+                                        </div>
+                                    </div>
+                                    <div class="col-md-3">
+                                        <div class="form-group">
+                                            <label>Postal Code</label>
+                                            <input type="text" name="u_zip" id="u_zip"
+                                                   class="form-control"
+                                                   placeholder="Enter postal code (only digits)"
+                                                   required>
+                                        </div>
+                                    </div>
+                                    <div class="col-md-3">
+                                        <div class="form-group">
+                                            <label>Phone</label>
+                                            <input type="text" name="u_phone" id="u_phone"
+                                                   class="form-control"
+                                                   placeholder="Enter phone (only digits)">
+                                        </div>
+                                    </div>
+                                    <div class="col-md-4">
+                                        <div class="form-group">
+                                            <label>Email</label>
+                                            <input type="email" name="u_email" id="u_email" class="form-control"
+                                                   placeholder="Enter email">
+                                        </div>
+                                    </div>
+                                    <div class="col-md-4">
+                                        <div class="form-group">
+                                            <label>Website</label>
+                                            <input type="url" name="u_website" id="u_website" class="form-control"
+                                                   placeholder="Enter website">
+                                        </div>
+                                    </div>
+                                    <div class="col-md-2">
+                                        <div class="form-group">
+                                            <label>Sector</label>
+                                            <select class="form-control select2 select2-hidden-accessible"
+                                                    style="width: 100%;"
+                                                    data-select2-id="1" tabindex="-1" aria-hidden="true"
+                                                    id="u_type" name="u_type">
+                                                <option selected="selected"> --Select--</option>
+                                                <option data-select2-id="105">Alaska</option>
+                                                <option data-select2-id="106">California</option>
+                                                <option data-select2-id="107">Delaware</option>
+                                                <option data-select2-id="108">Tennessee</option>
+                                                <option data-select2-id="109">Texas</option>
+                                                <option data-select2-id="110">Washington</option>
+                                            </select>
+                                        </div>
+                                    </div>
+                                    <div class="col-md-2">
+                                        <div class="form-group">
+                                            <label>Unit size</label>
+                                            <select class="form-control select2 select2-hidden-accessible"
+                                                    style="width: 100%;"
+                                                    data-select2-id="1" tabindex="-1" aria-hidden="true"
+                                                    id="u_size" name="u_size">
+                                                <option selected="selected"> --Select--</option>
+                                                <option data-select2-id="115">Option 1</option>
+                                            </select>
+                                        </div>
                                     </div>
                                 </div>
-                                <div class="col-md-6">
-                                    <div class="form-group">
-                                        <label>Address 1</label>
-                                        <input type="text" name="u_address1" class="form-control"
-                                               placeholder="Enter address"
-                                               required="required">
-                                    </div>
-                                    <div class="form-group">
-                                        <label>Address 2</label>
-                                        <input type="text" name="u_address2" class="form-control"
-                                               placeholder="Enter address">
-                                    </div>
-                                </div>
-                                <div class="col-md-4">
-                                    <div class="form-group">
-                                        <label>City</label>
-                                        <input type="text" name="u_city" class="form-control" placeholder="Enter city"
-                                               required="required">
-                                    </div>
-                                </div>
-                                <div class="col-md-2">
-                                    <div class="form-group">
-                                        <label>State</label>
-                                        <select class="form-control select2 select2-hidden-accessible"
-                                                style="width: 100%;"
-                                                data-select2-id="1" tabindex="-1" aria-hidden="true"
-                                                id="u_size" name="u_size">
-                                            <option selected="selected"> --Select--</option>
-                                            <option data-select2-id="115">Option 1</option>
-                                        </select>
-                                    </div>
-                                </div>
-                                <div class="col-md-3">
-                                    <div class="form-group">
-                                        <label>Postal Code</label>
-                                        <input type="text" pattern="[0-9]*" name="u_zip" class="form-control"
-                                               placeholder="Enter postal code (only numbers)"
-                                               required="required">
-                                    </div>
-                                </div>
-                                <div class="col-md-3">
-                                    <div class="form-group">
-                                        <label>Phone</label>
-                                        <input type="text" pattern="[0-9]*" name="u_phone" class="form-control"
-                                               placeholder="Enter phone (only numbers)">
-                                    </div>
-                                </div>
-                                <div class="col-md-4">
-                                    <div class="form-group">
-                                        <label>Email</label>
-                                        <input type="email" name="u_email" class="form-control"
-                                               placeholder="Enter email">
-                                    </div>
-                                </div>
-                                <div class="col-md-4">
-                                    <div class="form-group">
-                                        <label>Website</label>
-                                        <input type="url" name="u_website" class="form-control"
-                                               placeholder="Enter website">
-                                    </div>
-                                </div>
-                                <div class="col-md-2">
-                                    <div class="form-group">
-                                        <label>Sector</label>
-                                        <select class="form-control select2 select2-hidden-accessible"
-                                                style="width: 100%;"
-                                                data-select2-id="1" tabindex="-1" aria-hidden="true"
-                                                id="u_type" name="u_type">
-                                            <option selected="selected"> --Select--</option>
-                                            <option data-select2-id="105">Alaska</option>
-                                            <option data-select2-id="106">California</option>
-                                            <option data-select2-id="107">Delaware</option>
-                                            <option data-select2-id="108">Tennessee</option>
-                                            <option data-select2-id="109">Texas</option>
-                                            <option data-select2-id="110">Washington</option>
-                                        </select>
-                                    </div>
-                                </div>
-                                <div class="col-md-2">
-                                    <div class="form-group">
-                                        <label>Unit size</label>
-                                        <select class="form-control select2 select2-hidden-accessible"
-                                                style="width: 100%;"
-                                                data-select2-id="1" tabindex="-1" aria-hidden="true"
-                                                id="u_size" name="u_size">
-                                            <option selected="selected"> --Select--</option>
-                                            <option data-select2-id="115">Option 1</option>
-                                        </select>
-                                    </div>
+                                <div class="modal-footer justify-content-between">
+                                    <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
+                                    <input type="submit" class="btn btn-primary" id="save-unit-button">Save</input>
                                 </div>
                             </div>
-                            <div class="modal-footer justify-content-between">
-                                <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
-                                <button type="button" class="btn btn-primary">Save</button>
-                            </div>
                         </div>
+                        <!-- /.modal-content -->
                     </div>
-                    <!-- /.modal-content -->
-                </div>
-                <!-- /.modal-dialog -->
+                    <!-- /.modal-dialog -->
+                </form>
             </div>
             <!-- /.Left col -->
         </div>
@@ -163,10 +169,10 @@
                 <tbody>
                 <?php //foreach($units as $unit){ ?>
                 <tr>
-                    <td><?= ""; //$unit["id"];           ?></td>
-                    <td><?= ""; //$unit["name"];         ?></td>
-                    <td><?= ""; //$unit["address1"];         ?></td>
-                    <td><?= "";// $unit["city"];         ?></td>
+                    <td><?= ""; //$unit["id"];            ?></td>
+                    <td><?= ""; //$unit["name"];          ?></td>
+                    <td><?= ""; //$unit["address1"];          ?></td>
+                    <td><?= "";// $unit["city"];          ?></td>
                     <td>CA</td>
                     <td>Names</td>
                     <td>4444444444</td>
---
+++ public/assets/js/validation.js
@@ -0,0 +1,80 @@
+$(function() {
+
+    $.validator.addMethod('')
+
+    $("#units-form").validate({
+        rules: {
+            u_name: {
+                required: true,
+                maxlength: 2
+            },
+            u_address1: {
+                required: true
+            },
+            u_city: {
+                required: true
+            },
+            u_state: {
+                required: true
+            },
+            u_zip: {
+                required: true,
+                digits: true
+            },
+            u_phone: {
+                required: false,
+                digits: true
+            },
+            u_email: {
+                required: false,
+                email: true
+            },
+            u_website: {
+                required: false,
+                url: true
+            },
+            u_type: {
+                required: false
+            },
+            u_size: {
+                required: false
+            }
+        },
+
+        messages: {
+            u_name: {
+                required: 'Please enter a name.',
+                maxlength: 'Please less than 2.'
+            },
+            u_address1: {
+                required: 'Please enter an address.'
+            },
+            u_city: {
+                required: 'Please enter city.'
+            },
+            u_state: {
+                required: 'Please select state.'
+            },
+            u_zip: {
+                required: 'Please enter postal code.',
+                digits: 'Please enter digits only.'
+            },
+            u_phone: {
+                digits: 'Please enter digits only.'
+            },
+            u_email: {
+                email: 'Please enter a <em>valid</em> email address.'
+            },
+            u_website: {
+                url: 'Please enter a <em>valid</em> URL.'
+            },
+            u_type: {
+                required: 'Please select sector.'
+            },
+            u_size: {
+                required: 'Please select size range.'
+            }
+        }
+
+    });
+});

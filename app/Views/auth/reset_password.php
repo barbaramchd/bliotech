abdeee083b5b0cf2d7a95345cd3850fcb81e14f4
jjur <juraj.vasek@minerva.kgi.edu>
2021-05-22 23:25:57 +0200

    Password reset

--- app/Views/auth/reset_password.php
+++ app/Views/auth/reset_password.php
@@ -1,21 +1,46 @@
-<h1><?php echo lang('Auth.reset_password_heading');?></h1>
+<?php echo view("auth/header"); ?>
+<body class="hold-transition login-page">
+<div class="login-box">
+    <div class="login-logo">
+        <a href="./index.html"><b>Blio</b>Tech</a>
+    </div>
+    <!-- /.login-logo -->
+    <div class="card">
+        <div class="card-body login-card-body">
+            <p class="login-box-msg">Create new password</p>
+            <div id="infoMessage"><?php echo $message;?></div>
+            <form action="<?= base_url("auth/reset_password/".$code);?>" method="post">
+                <div class="input-group mb-3">
+                    <input name="new" type="password" class="form-control" placeholder="Password">
+                    <div class="input-group-append">
+                        <div class="input-group-text">
+                            <span class="fas fa-lock"></span>
+                        </div>
+                    </div>
+                </div>
 
-<div id="infoMessage"><?php echo $message;?></div>
+                <div class="input-group mb-3">
+                    <input name="new_confirm" type="password" class="form-control" placeholder="Password">
+                    <div class="input-group-append">
+                        <div class="input-group-text">
+                            <span class="fas fa-lock"></span>
+                        </div>
+                    </div>
+                </div>
+                <?php echo form_input($user_id);?>
+                <div class="row">
+                    <div class="col-12">
+                        <button type="submit" class="btn btn-primary btn-block">Request new password</button>
+                    </div>
+                    <!-- /.col -->
+                </div>
+            </form>
 
-<?php echo form_open('auth/reset_password/' . $code);?>
 
-	<p>
-		<label for="new_password"><?php echo sprintf(lang('Auth.reset_password_new_password_label'), $minPasswordLength);?></label> <br />
-		<?php echo form_input($new_password);?>
-	</p>
+        </div>
+        <!-- /.login-card-body -->
+    </div>
+</div>
+<!-- /.forgot_password-box -->
+<?php echo view("auth/footer"); ?>
 
-	<p>
-		<?php echo form_label(lang('Auth.reset_password_new_password_confirm_label'), 'new_password_confirm');?> <br />
-		<?php echo form_input($new_password_confirm);?>
-	</p>
-
-	<?php echo form_input($user_id);?>
-
-	<p><?php echo form_submit('submit', lang('Auth.reset_password_submit_btn'));?></p>
-
-<?php echo form_close();?>
